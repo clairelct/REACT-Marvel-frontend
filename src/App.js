27c6362a9cb987fc99775e50f49b9a4b009a32b4
Claire Leconte <claire.lcnt@gmail.com>
2020-11-18 19:56:59 +0100

    Initial commit, set route Characters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Characters from "./containers/Characters";
 
 function App() {
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/characters">
+          <Characters></Characters>
+        </Route>
+        <Route path="/">
+          <p>Home</p>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
